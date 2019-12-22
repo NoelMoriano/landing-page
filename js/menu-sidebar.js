2ef953e7e22ae9f6988c64e13653a82b4c3a453b
noelmoriano <mariano260996@gmail.com>
2019-12-22 14:27:50 -0500

    üpdate colors

--- js/menu-sidebar.js
+++ js/menu-sidebar.js
@@ -6,9 +6,5 @@ function sidebar_open() {
 console.log(window.screen.width);
 
 function sidebar_close() {
-  if(window.screen.width >= 1366){
-  		document.getElementById("mySidebar").style.display = "visible";
-  }else{
   		document.getElementById("mySidebar").style.display = "none";
-  }
 }
