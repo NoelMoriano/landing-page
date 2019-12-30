40dd9743ec83ee2f1873266fe7a1c6c805e9ad40
noelmoriano <mariano260996@gmail.com>
2019-12-30 13:13:29 -0500

    update config for wordpress

--- template-blogs.php
+++ template-blogs.php
@@ -17,51 +17,18 @@
 		<div class="container-blog-articles">
 			<div class="content-items-articles">
 
+<?php while (have_posts()) : the_posts();?>
 				<div class="item-article">
 					<div class="img-item">
-						<a href="#"><img class="img-article" src="<?php bloginfo('template_directory');?>/images/article-blank.png" alt="Img-article"></a>
+						<a href="<?php the_permalink();?>"><img class="img-article" src="<?php the_posts_thumbnails('full');?>" alt="Img-article"></a>
 					</div>
 					<div class="description-item">
-						<a href="#"><span class="title-article">Articulo 1</span></a>
-						<span class="info-article">Autor / 01-01-2000 / Categoría</span>
-						<p class="description-article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, inventore dolore voluptas excepturi doloremque corporis quos dolor maiores voluptatum reiciendis aspernatur odio impedit qui facere, illo. Minima molestias atque alias.</p>
+						<a href="<?php the_permalink();?>"><span class="title-article"><?php the_title();?></span></a>
+						<span class="info-article"><?php the_author_posts_link();?> / <?php the_time('F jS, Y');?> / <?php the_comments_number('0 Comments','1 Comments','% responses');?></span>
+						<p class="description-article"><?php the_content('Read More'); ?></p>
 					</div>
 				</div>
-
-				<div class="item-article">
-					<div class="img-item">
-						<a href="#"><img class="img-article" src="<?php bloginfo('template_directory');?>/images/article-blank.png" alt="Img-article"></a>
-					</div>
-					<div class="description-item">
-						<a href="#"><span class="title-article">Articulo 2</span></a>
-						<span class="info-article">Autor / 01-01-2000 / Categoría</span>
-						<p class="description-article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, inventore dolore voluptas excepturi doloremque corporis quos dolor maiores voluptatum reiciendis aspernatur odio impedit qui facere, illo. Minima molestias atque alias.</p>
-					</div>
-				</div>
-
-				<div class="item-article">
-					<div class="img-item">
-						<a href="#"><img class="img-article" src="<?php bloginfo('template_directory');?>/images/article-blank.png" alt="Img-article"></a>
-					</div>
-					<div class="description-item">
-						<a href="#"><span class="title-article">Articulo 3</span></a>
-						<span class="info-article">Autor / 01-01-2000 / Categoría</span>
-						<p class="description-article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, inventore dolore voluptas excepturi doloremque corporis quos dolor maiores voluptatum reiciendis aspernatur odio impedit qui facere, illo. Minima molestias atque alias.</p>
-					</div>
-				</div>
-
-
-				<div class="item-article">
-					<div class="img-item">
-						<a href="#"><img class="img-article" src="<?php bloginfo('template_directory');?>/images/article-blank.png" alt="Img-article"></a>
-					</div>
-					<div class="description-item">
-						<a href="#"><span class="title-article">Articulo 4</span></a>
-						<span class="info-article">Autor / 01-01-2000 / Categoría</span>
-						<p class="description-article">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, inventore dolore voluptas excepturi doloremque corporis quos dolor maiores voluptatum reiciendis aspernatur odio impedit qui facere, illo. Minima molestias atque alias.</p>
-					</div>
-				</div>
-
+<?php endwhile; ?>
 
 			</div>
 		</div>
