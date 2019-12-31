9b39624744eaae50981f19f7c72f5564c04725f1
noelmoriano <mariano260996@gmail.com>
2019-12-31 01:55:57 -0500

    update config for wordpress

---
+++ page_entradas.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ Template Name: Page Entradas
+ Template Post Type: post
+ */
+	get_header();
+?>
+
+<section class="content-blog-page">
+		<div class="container-articles-content">
+		<div class="container-titles">
+			<div class="container-title-one">
+				<span class="text-small">Compartimos</span>
+				<span class="text-title">NUESTRA EXPERIENCIA</span>
+			</div>
+		</div>
+		<div class="container-blog-articles">
+			<div class="content-items-articles">
+
+<?php while (have_posts()) : the_posts();?>
+				<div class="item-article">
+				
+					<div class="description-item">
+						<a href="#"><span class="title-article"><?php the_archive_title();?></span></a>
+			
+						<p class="description-article"><?php the_archive_description(); ?></p>
+					</div>
+				</div>
+<?php endwhile; ?>
+
+			</div>
+		</div>
+		</div>
+	</section>
+
+
+
+	<?php get_footer(); ?>
